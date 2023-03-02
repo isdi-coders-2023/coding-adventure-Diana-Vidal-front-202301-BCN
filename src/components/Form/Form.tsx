5495625923a46281f4f805657bcce66edcf02a76
Diana Vidal <dvidaal@gmail.com>
2023-03-02 12:49:44 +0100

    Create functions to give functionality to forms

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import Button from "../Button/Button";
 import FormStyled from "./FormStyled";
 
 const Form = (): JSX.Element => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <FormStyled>
       <label>
         Email
-        <input type="email" placeholder="Introduce your email" />
+        <input
+          type="email"
+          placeholder="Introduce your email"
+          value={email}
+          onChange={handleEmailChange}
+        />
         <span
           hidden
           className="form__required-text form__required-text--hidden"
@@ -16,7 +33,12 @@ const Form = (): JSX.Element => {
       </label>
       <label>
         Password
-        <input type="password" placeholder="Introduce your password" />
+        <input
+          type="password"
+          placeholder="Introduce your password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
       </label>
       <label>
         Image
